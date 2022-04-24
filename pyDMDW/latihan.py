e69993c90af703a6d27d403e8f4c48e492f3c6bf
Harfel12 <85319667+harfely@users.noreply.github.com>
2022-04-25 06:12:42 +0700

    update file

--- pyDMDW/latihan.py
+++ pyDMDW/latihan.py
@@ -1,1 +1,15 @@
+import matplotlib
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from IPython.core.display import HTML
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 print ("Hello World")
+y = 'Saya'
+x = 'suka'
+z = 'Belajar Python'
+print (y, x, z)
